d2ff352c3d57885578270fd93e7d86a6875766f6
Robert Sun <sunwrobert@gmail.com>
2016-10-04 00:11:23 -0400

    Added locations and filter markers

--- projects/neighborhood-map/js/app.js
+++ projects/neighborhood-map/js/app.js
@@ -1,58 +1,114 @@
 var locations = [{
     title: "The Varsity",
-    location: {
-        lat: 0,
-        lng: 0
+    position: {
+        lat: 33.771510,
+        lng: -84.389311
     }
 }, {
-    title: "The Dome",
-    location: {
-        lat: 0,
-        lng: 0
+    title: "Georgia Aquarium",
+    position: {
+        lat: 33.763424,
+        lng: -84.394891
     }
 }, {
-    title: "The Wonder",
-    location: {
-        lat: 0,
-        lng: 0
+    title: "Taco Mac",
+    position: {
+        lat: 33.780643,
+        lng: -84.383911
     }
 }, {
-    title: "The Place",
-    location: {
-        lat: 0,
-        lng: 0
+    title: "Antico's Pizza",
+    position: {
+        lat: 33.784651,
+        lng: -84.405589
     }
 }, {
-    title: "The Wooters",
-    location: {
-        lat: 0,
-        lng: 0
+    title: "Five Guys Burger and Fries",
+    position: {
+        lat: 33.778194,
+        lng: -84.385114
     }
 }, {
-    title: "The Pumpkins",
-    location: {
-        lat: 0,
-        lng: 0
+    title: "Georgia Tech Campus Recreation Center",
+    position: {
+        lat: 33.775525,
+        lng: -84.403912
     }
 }, ];
 
-var markers = [];
+var config = {
+    apiKey: 'XXXXXXXXXXXXXX',
+    authUrl: 'https://foursquare.com/',
+    apiUrl: 'https://api.foursquare.com/'
+  };
 
-function init(){
+function init() {
     initEventListeners();
     initMap();
 }
 
 var map;
+var markers;
+var latlngbounds;
 
 function initMap() {
+    latlngbounds = new google.maps.LatLngBounds();
     map = new google.maps.Map(document.getElementById('map'), {
         center: {
-            lat: -34.397,
-            lng: 150.644
+            lat: 33.7490,
+            lng: -84.3880
         },
         zoom: 8,
         mapTypeControl: false
+    });
+
+
+    addMarkers();
+
+    map.fitBounds(latlngbounds);
+}
+
+function clickLocation(location) {
+    // Use closure to save the reference to marker
+    return function () {
+        if (location.marker.getAnimation() !== null) {
+            location.infoWindow.close();
+            location.marker.setAnimation(null);
+        } else {
+            location.infoWindow.open(map, location.marker);
+            location.marker.setAnimation(google.maps.Animation.BOUNCE);
+        }
+    };
+
+}
+
+function addMarkers() {
+    locations.forEach(function (location) {
+        var contentString = '<h1>' + location.title + '</h1>'
+        var infoWindow = new google.maps.InfoWindow({
+            content: contentString
+        });
+
+        latlngbounds.extend(location.position);
+        marker = new google.maps.Marker({
+            map: map,
+            draggable: false,
+            animation: google.maps.Animation.DROP,
+            position: location.position
+        });
+
+        // Here, we want to add a reference to each marker and info window with its corresponding location so we can refer to them later.
+        location.marker = marker;
+        location.infoWindow = infoWindow;
+
+        // Stop the marker from animating if the info window button is closed.
+        google.maps.event.addListener(location.infoWindow, 'closeclick', function(){
+            if(location.marker.getAnimation() !== null){
+                location.marker.setAnimation(null);
+            }
+        })
+
+        location.marker.addListener('click', clickLocation(location));
     });
 }
 
@@ -75,14 +131,33 @@ function toggleOpen(ele) {
 
 var ViewModel = function () {
     var self = this;
-    this.locations = ko.observable(locations);
+    this.locations = ko.observableArray(locations);
     this.filter = ko.observable("");
-    console.log(this.locations());
     this.filteredLocations = ko.computed(function () {
         return ko.utils.arrayFilter(self.locations(), function (location) {
             return location.title.toUpperCase().includes(self.filter().toUpperCase());
         });
     });
+    this.noMatchesFound = ko.computed(function () {
+        return self.filteredLocations().length == 0;
+    });
+    this.clickLocation = function (location) {
+        return function (location) {
+            clickLocation(location)();
+        };
+    };
+
+    // Duplicated code because filtered locations wouldn't include the markers in each location.
+    this.filterMarkers = function(){
+        this.locations().forEach(function(location){
+            if(location.title.toUpperCase().includes(self.filter().toUpperCase())){
+                location.marker.setMap(map);
+            }
+            else {
+                location.marker.setMap(null);
+            }
+        })
+    }
 };
 
 
